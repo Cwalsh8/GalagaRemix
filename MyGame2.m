63168b74aaaf0ca44d17f34d359b92f2ea9fca5a
Cwalsh8 <Cwalsh8@student.gsu.edu>
2016-11-28 21:36:08 -0500

    Finished Game
    -Put hit detection in.
    -Sound effects on shooting/getting hit/winning & lose.
    -Game Over and Winning Text
    -Scoreboard at the bottom.

--- MyGame2.m
+++ MyGame2.m
@@ -4,11 +4,11 @@
     clc;
     
 %Main program
-%Note: Can't move up and down in Real Galaga...might change ours
-%Need to properly delete the bullets to prevent memory leaks
-%Then work on hit detection.
 
-    fig = figure('Color','black'); 
+    fig = figure('Color','black');  %Creates the figure with a black background
+    
+    %Creates global variables for use inside functions as well as outside functions
+    
     global x; 
     global y;
     global ship;
@@ -18,32 +18,55 @@
     global gameOver;
     global frames;
     global shooterFire;
+    
+    %Initializing global variables
+    
     x = 5; 
-    y = 0;
-    ship = plot(x,y,'*'); 
+    y = 1;
+    ship = plot(x,y,'*','Color','green'); 
     count = 0;
     bullet = [];
     shooterFire = [];
     gameOver = false;
     frames = 0;
-    title('Galaga Remix', 'Color', [0.5 0 0.5]);
-    axis([0,10,0,10]);
-    hold on %Keeps the axis the same when plotting/setting additional points
-    set(gca,'Color','black'); 
+    
+    
+    title('Galaga Remix','Color',[0.5 0 0.5]);
+    axis([0,10,0,10]); %Sets each axis from 0 to 10.
+    hold on %Keeps the axis in the same position when plotting points
+    set(gca,'Color','black'); %Makes current axes black
+    
+    
+    %Turns off the unnessecary graph items for this game
+    
+    box off;
+    set(gca,'xcolor',get(gcf,'color')); 
+    set(gca,'xtick',[]);
+    set(gca,'ycolor',get(gcf,'color'));
+    set(gca,'ytick',[])
+    
+    %Plots all the enemies into their intial places.
+    
     for i=1:4
         for j=2:8
-            enemy(i,j) = plot(j,10-i,'x','Color','r'); %Enemy's are going to be plotted by row.
+            enemy(i,j) = plot(j,10-i,'x','Color','r'); 
         end
     end
   
-    goRight = true; %Enemies start off moving to the Right
-    num = 0;
-    while ~gameOver %Main game loop
-        frames = frames + 1;
+    goRight = true; %Boolean to check if enemies should move left or right 
+    num = 0; %count for keeping track of enemy ship array
+    
+    %Main game loop, 
+    %Each loop through represents a frame
+    
+    while ~gameOver 
+        frames = frames + 1; 
+        xlabel({'Score:';(100000-frames)},'Color',[0.5 0 0.5]);
         set(fig,'KeyPressFcn',@getKeys); %Sets the figure so on a keypress, the function @getKeys runs
         
         %This block makes the enemies sway left and right
-        if (mod(frames,8) == 0)
+        
+        if (mod(frames,32) == 0)
             lastShipX = get(enemy(:,size(enemy,2)), 'XData');
             lastShipXX = cell2mat(lastShipX); 
             firstShipX = get(enemy(:,2), 'XData');
@@ -59,22 +82,44 @@
                 end
             end
             if goRight
+                test = 0;
                 for i=1:4
                     for j=2:8
-                        set(enemy(i,j),'XData', get(enemy(i,j),'XData')+1);                     
+                        set(enemy(i,j),'XData', get(enemy(i,j),'XData')+1);
+                        if get(enemy(i,j),'YData') == -10
+                            test = test + 1;
+                            if test == 28
+                                gameOver = true;
+                                text(4.2,5,'Congratulations!','Color','y','FontSize',25);
+                                [t, Fs] = audioread('coin_credit.wav');
+                                sound(t, Fs);
+                            end
+                        end
                     end
                 end
             else 
+                test = 0;
                 for i=1:4
                     for j=2:8
                         set(enemy(i,j),'XData', get(enemy(i,j),'XData')-1); 
+                        if get(enemy(i,j),'YData') == -10
+                            test = test + 1;
+                            if test == 28
+                                gameOver = true;
+                                text(4.2,5,'Congratulations!','Color','y','FontSize',25);
+                                [t, Fs] = audioread('coin_credit.wav');
+                                sound(t, Fs);
+                            end
+                        end
                     end
                 end
             end
         end
         
-        %This block will select a random enemy every 10 frames
-        if (mod(frames,30) == 0 || frames == 1)
+        %This block will select a random enemy to shoot every 20-40 frames
+        
+        randFire = randi([20,40],1); 
+        if (mod(frames,randFire) == 0 || frames == 1)
             num = num + 1;
             randNum = randi([1 i],1);
             randNum = [randNum randi([2 j],1)];
@@ -84,29 +129,57 @@
             shooterFire(num) = plot(shooterCoords(1), shooterCoords(2), 'v', 'Color', 'y');
         end
         
-        %This sets up the bullets position in the next frames, then draws it
+        %This block sets up our enemies' bullets position for the next frame
+        
         for o = 1:length(shooterFire)
             if ~isnan(shooterFire(o))
-                set(shooterFire(o), 'YData', get(shooterFire(o), 'YData') - .1);
-                if (get(shooterFire(o), 'YData') < 0)
+                a = get(shooterFire(o), 'YData');
+                z = get(shooterFire(o), 'XData');
+                set(shooterFire(o), 'YData', a - .1);
+                if eq(single(a), single(y)) && eq(single(z),single(x)) 
+                    xlabel({'Score:';0},'Color',[0.5 0 0.5]);
+                    gameOver = true;
+                    text(4.2,5,'GAME OVER','Color','red','FontSize',25);
+                    [t, Fs] = audioread('fighter_destroyed.wav');
+                    sound(t, Fs);
+                elseif a < -1
                     shooterFire(o) = NaN;
-                    disp(shooterFire(:));
                 end
             end
         end
+        
+        %This block sets up our bullets position for the next frame
+        
+        
         for p = 1:length(bullet)
             if ~isnan(bullet(p))
-                set(bullet(p), 'YData', get(bullet(p), 'YData') + .1);
+                b = get(bullet(p), 'YData');
+                c = get(bullet(p), 'XData');
+                set(bullet(p), 'YData', get(bullet(p), 'YData') + .25);
+                for m=1:4
+                    for n=2:8
+                        q = get(enemy(m,n), 'YData');
+                        r = get(enemy(m,n), 'XData');
+                        if eq(single(q), single(b)) && eq(single(c), single(r))
+                            %TEMPORARY THROW ENEMY OFF SCREEN
+                            set(enemy(m,n), 'YData', -10);
+                            set(bullet(p), 'XData', 30);
+                            [t, Fs] = audioread('explosion.wav');
+                            sound(t, Fs);
+                        end
+                    end
+                end
                 if (get(bullet(p), 'YData') > 10)
                     bullet(p) = NaN;
-                    disp(bullet(:));
-                end
+                end          
             end
         end
+        
+        %At the end of each frame, it draws all the new positions of each object at once.
         drawnow;
         
-        %This pause controls how fast the game loop
-        pause(.05); 
+        %This pause controls how fast the game iterates through all these operations.
+        pause(.005);
     end
     
     
@@ -159,17 +232,12 @@
         bulletG = bullet;
     end    
     
-    function enemyG = getEnemy
-        global enemy
-        enemyG = enemy;
-    end
-    
 %List of functions
     
-    %Returns the modifier and key pressed by user. 
-    %Also moves the ship depending on what was pressed. 
+    %Returns the key pressed by user. 
+    %Also moves the ship/plot bullets depending on what was pressed.
+    
     function getKeys (fig,evt)
-        mod = evt.Modifier;
         keyPressed = evt.Key;
         xG = getGlobalx;
         yG = getGlobaly;
@@ -198,7 +266,7 @@
             set(shipG, 'XData', getGlobalx, 'YData', getGlobaly);
         end
         if (strcmpi(keyPressed, 'downarrow'))
-            if ( yG > 0 )
+            if ( yG > 1 )
                 yG = yG-1;
             end
             setGlobaly(yG);
@@ -206,20 +274,10 @@
         end
         if (strcmpi(keyPressed, 'space'))
             countG = countG + 1;
-            bulletG(countG) = plot(getGlobalx, getGlobaly+1, '^', 'Color', 'blue');
+            bulletG(countG) = plot(getGlobalx, getGlobaly+1, '^', 'Color', 'cyan');
             setCount(countG);
             setBullet(bulletG);
-                   %Under this is my hit detection test
-                   %try
-                   %   if (yBulletG(i)==get(enemy1G, 'YData') && xBulletG(countG)==get(enemy1G, 'XData') )
-                   %      delete(enemy1G);
-                   %  end
-                   %catch
-                        %disp('It cant find the deleted ship but who cares');
-                   %end
-                   %Going to write a try-catch-continue for now to ignore error message
-                   %Need to re-wrire this all properly, but it does delete the object like I want.
-                %end
-            %end    
+            [t, Fs] = audioread('laser_widebeam.wav');
+            sound(t, Fs);
         end
     end 
